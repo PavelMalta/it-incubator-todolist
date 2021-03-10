f08feb7a55c917af1155b9326f8ec547038657b6
PavelMalta <shamashov87@mail.ru>
2021-03-10 11:36:08 +0300

    add CreateTodolist in todolists-api.stories, lesson 13

--- src/todolists-api.stories.tsx
+++ src/todolists-api.stories.tsx
@@ -6,7 +6,10 @@ export default {
 }
 
 const settings = {
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'API-KEY': '5adec434-290c-4eac-9a59-2a005f8dd341'
+    }
 }
 
 
@@ -24,13 +27,17 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists',
+            {title: "newTodolist"}, settings).then((res) => {setState(res.data)})
     }, [])
+
 
     return <div> {JSON.stringify(state)}</div>
 }
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
