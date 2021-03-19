0af2e2ff45c81ed9a7771ca2123c18a72296553b
PavelMalta <shamashov87@mail.ru>
2021-03-19 20:43:21 +0300

    add app-reducer and progress indicator

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css'
-import {AppBar, Button, Container, IconButton, Toolbar, Typography} from '@material-ui/core'
+import {AppBar, Button, Container, IconButton, LinearProgress, Toolbar, Typography} from '@material-ui/core'
 import {Menu} from '@material-ui/icons'
 import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "./store";
 
 function App() {
+
+    const status = useSelector<AppRootStateType>((state) => state.app.status)
 
     return (
         <div className="App">
@@ -19,6 +23,7 @@ function App() {
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
+            { status === 'loading' && <LinearProgress color={"secondary"}/>}
             <Container fixed>
                 <TodolistsList/>
             </Container>
